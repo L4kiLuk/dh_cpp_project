9f6f8d20990a7894d29e27a8452cdb81aef40c5b
L4kiLuk <lu.burkhardt@protonmail.com>
2022-12-23 18:09:59 +0100

    add menu

--- controller.cpp
+++ controller.cpp
@@ -19,10 +19,23 @@ void Controller::start(){
     //Print Welcome message
     std::cout << welcomeMessage();
     //Open the Input
-    std::string inputCommand;
+    char inputCommand;
     while(1){
         std::cin >> inputCommand;
-        //Handle the Menu
+        //Handle the menu
+        switch(inputCommand){
+            case '1':
+                //New Game
+                std::cout << "New Game"<< std::endl;
+                break;
+            case '2':
+                //Load Autosave
+                std::cout << "Load Autosave"<< std::endl;
+                break;
+            default:
+                std::cout <<"Bitte gib eine der oberen Möglichkeiten ein!"<<std::endl;
+            
+        }
     }
     //Handle manipulations(new Game||load Game||autosave)
     //Start the game
