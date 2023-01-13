2522e668a2462692a1d0d50fb26eb3f5a2df55b0
L4kiLuk <lu.burkhardt@protonmail.com>
2023-01-13 14:32:29 +0100

    code cleanup

--- controller.cpp
+++ controller.cpp
@@ -8,22 +8,23 @@
 #include "view.cpp"
 #include "robot.cpp"
 
+//Constructor
 Controller::Controller(){
-    view = new View(&matchfield);
-    
+    view = new View(&matchfield); 
 }
-
+//Deconstructor
 Controller::~Controller(){
     //Autosave the Game
     delete view;
     delete matchfield;
 }
-
-void Controller::init(){
+//Start the controller 
+void Controller::start(){
+    //Init signalhandler
     sigObj=&matchfield;
     std::signal(SIGINT,signal_handler);
-    //Print Welcome message
     view->printWelcomeMessage();
+    //Look for autosave
     std::ifstream ifs(AUTOSAVEFILE);
     if(ifs.good()){
         ifs.close();
@@ -34,66 +35,59 @@ void Controller::init(){
             loadGame(AUTOSAVEFILE);
         }
     }
-    //Open the Input
+    //Handle the menu
     char inputCommand;
     while(1){
         view->printMenu();
         std::cin >> inputCommand;
         //Handle the menu
         switch(inputCommand){
-            case '1':
-                //New Game
+            case '1':   //New game
                 newGame();
                 break;
-            case '2':
-                //Load Savegame
-                {
-                    std::cout<<"Gib eine Datei an!"<<std::endl;
+            case '2':   //Load savegame
+                {view->printLine("Gib eine Datei an!");
                     std::string file;
                     std::cin >> file;
-                    loadGame(file);
-                }
+                    loadGame(file);}
                 break;
-            case '3':
+            case '3': //Exit the Program
                 exit(1);
                 break;
-            case '4':
+            case '4':   //Show the highscores
                 {view->printHighscore(loadHighscores());
                 std::string s;
                 std::getline(std::cin,s);
                 std::getline(std::cin,s);}
                 break;
             default:
-                std::cout <<"Bitte gib eine der oberen Möglichkeiten ein!"<<std::endl;
+                view->printLine("Bitte gib eine der oberen Möglichkeiten ein!");
             
         }
     }
     
 
 }
-
+//Starts a new game
 void Controller::newGame(){
-    //create new Spielfeld
     matchfield = new Matchfield;
-    //init it
     matchfield->init();
-    //start game
     startGame();
 }
-
+//Load a game from a file and stats it
 void Controller::loadGame(std::string file){
     std::ifstream ifs(file);
     matchfield = new Matchfield;
-    char nextchar;
-    ifs >> nextchar;
-    matchfield->setActualPlayer(nextchar-'0');
+    char nextChar;
+    ifs >> nextChar;
+    matchfield->setActualPlayer(nextChar-'0');
     for(int x=0;x<8;x++){
         for(int y=0;y<8;y++){
-            ifs>>nextchar;
-            if(nextchar!='#'){
-                bool black = nextchar-'0';
-                ifs>>nextchar;
-                bool state = nextchar-'0';
+            ifs>>nextChar;
+            if(nextChar!='#'){
+                bool black = nextChar-'0';
+                ifs>>nextChar;
+                bool state = nextChar-'0';
                 matchfield->field[x][y]= new Stone(state,black);
             }
         }
@@ -106,46 +100,45 @@ void Controller::loadGame(std::string file){
     
     
 }
-
+//Save a running game to a file
 void Controller::saveGame(std::string file){
     std::ofstream ostream(file+".sgf");
     ostream<<(*Controller::sigObj)->getActualPlayer()<<std::endl;
-    for(int x=0;x<8;x++){
-        for(int y=0;y<8;y++){
-            if((*Controller::sigObj)->field[x][y]==NULL){
+    for(int column=0;column<8;column++){
+        for(int row=0;row<8;row++){
+            if((*Controller::sigObj)->field[column][row]==NULL){
                 ostream<<"#";
             }else {
-                ostream<<(*Controller::sigObj)->field[x][y]->black<<(*Controller::sigObj)->field[x][y]->state;
+                ostream<<(*Controller::sigObj)->field[column][row]->black<<(*Controller::sigObj)->field[column][row]->state;
             }
         }
         ostream << std::endl;
     }
     ostream.close();
 }
-
+//Start the Game and handle while running
 void Controller::startGame(){
-    std::cout << "Möchtest du gegen den Computer Spielen? y/n"<<std::endl;
+    std::string inputString;
     char in;
+    Robot robot = Robot(&matchfield);
+    std::regex move_pat("move [a-h][1-8] [a-h][1-8]");
+    std::regex hint_pat("hint [a-h][1-8]");
+    std::regex help_pat("help");
+    std::regex save_pat("save");
+    std::regex exit_pat("exit");
+    view->printLine("Möchtest du gegen den Computer Spielen? y/n");
     std::cin >> in;
     if(in='y'){
         withComputer=true;
     }
-    bool isRunning = true;
-    std::string nextMove;
-    std::getline(std::cin,nextMove);
-    Robot robot = Robot(&matchfield);
-    matchfield->changeActualPlayer();
+    std::getline(std::cin,inputString);//Clear the line
+    matchfield->changeActualPlayer();//White begins
     view->render();
-    while(isRunning){
-        std::getline(std::cin,nextMove);
-        std::regex move_pat("move [a-h][1-8] [a-h][1-8]");
-        std::regex hint_pat("hint [a-h][1-8]");
-        std::regex help_pat("help");
-        std::regex save_pat("save");
-        std::regex exit_pat("exit");
-        if(std::regex_match(nextMove,move_pat)){
+    while(1){
+        std::getline(std::cin,inputString);
+        if(std::regex_match(inputString,move_pat)){
             try{
-                matchfield->move(Coordinates_t(7-(nextMove.at(6)-'1'),nextMove.at(5)-'a'),Coordinates_t(7-(nextMove.at(9)-'1'),nextMove.at(8)-'a'));
+                matchfield->move(Coordinates_t(7-(inputString.at(6)-'1'),inputString.at(5)-'a'),Coordinates_t(7-(inputString.at(9)-'1'),inputString.at(8)-'a'));
                 view->render("Zug erfolgreich!");
                 if(withComputer&&matchfield->getActualPlayer()==0){
                     robot.nextMove();
@@ -154,31 +147,30 @@ void Controller::startGame(){
             }catch(WrongMoveException ex){// Fehlerhafter Zug
                 view->render(ex.info());
             }catch(GameEndException){//Spielende
-                isRunning=false;
                 view->printVictory();
-                //Save Highscore
-                char input;
-                std::cin >> input;
-                if(input == 'y'){
-                    std::cout<<"Gib einen Namen ein!"<<std::endl;
-                    std::cin>>nextMove;
-                    saveHighscore(nextMove,10);
+                std::cin >> in;
+                if(in == 'y'){
+                    view->printLine("Gib einen Namen ein!");
+                    std::cin>>inputString;
+                    saveHighscore(inputString,10);
                 }
+                break;
             }
         }
-        else if(std::regex_match(nextMove,hint_pat)){
-            view->render("Das sind die Mögliche Züge!",matchfield->hint(Coordinates_t(7-(nextMove.at(6)-'1'),nextMove.at(5)-'a')));
+        else if(std::regex_match(inputString,hint_pat)){
+            view->render("Das sind die Mögliche Züge!",matchfield->hint(Coordinates_t(7-(inputString.at(6)-'1'),inputString.at(5)-'a')));
         }
-        else if (std::regex_match(nextMove,help_pat)){
+        else if (std::regex_match(inputString,help_pat)){
             view->printHelp();
         }
-        else if (std::regex_match(nextMove,save_pat)){
-            std::cout<<"Gibt einen Namen für die Datei ein:"<<std::endl;
-            std::cin>> nextMove;
-            saveGame(nextMove);
+        else if (std::regex_match(inputString,save_pat)){
+            view->printLine("Gibt einen Namen für die Datei ein:");
+            std::getline(std::cin,inputString);
+            saveGame(inputString);
+            view->render("Gespeichert");
         }
-        else if (std::regex_match(nextMove,exit_pat)){
-            isRunning=false;
+        else if (std::regex_match(inputString,exit_pat)){
+            break;
         }
         else{
             view->render("Falscher Befehl!");
@@ -188,14 +180,14 @@ void Controller::startGame(){
     //Aufräumarbeiten
     delete matchfield;
 }
-
+//Save a new score to the high score file
 void Controller::saveHighscore(std::string playerName,int score){
     std::ofstream ostream(HIGHSCOREFILE,std::ios::app);
     std::time_t time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     ostream << std::ctime(&time) <<" "<< playerName <<" "<<score <<std::endl;
     ostream.close();
 }
-
+//Retuns all entry's in the high score file
 std::vector<Highscore> Controller::loadHighscores(){
     std::vector<Highscore> highscores;
     std::ifstream istream(HIGHSCOREFILE);
@@ -212,7 +204,7 @@ std::vector<Highscore> Controller::loadHighscores(){
     istream.close();
     return highscores;
 }
-
+//Make a autosave if SIGINIT is raised
 void signal_handler(int signal){
     std::ofstream ostream(AUTOSAVEFILE);
     ostream<<(*Controller::sigObj)->getActualPlayer()<<std::endl;
